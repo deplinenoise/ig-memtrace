f03ad8f0e388894ecd13f31a0de72be1a5094a3f
gjaegy <g.jaegy@imagine3d.fr>
2020-02-06 15:34:07 +0100

    significantly improved trace recording performance

--- tool/MemTrace/TraceTranscoder.cs
+++ tool/MemTrace/TraceTranscoder.cs
@@ -35,6 +35,7 @@ namespace MemTrace
     public const uint StreamMagic = 0xbfaf0003;
 
     public TraceMeta MetaData { get; private set; }
+    public HashSet<ulong> SymbolsHashSet { get; internal set; } = new HashSet<ulong>();
 
     private const uint BufferSize = 128 * 1024;
     private const uint RingMask = BufferSize - 1;
@@ -405,7 +406,7 @@ namespace MemTrace
               }
               else
               {
-                MetaData.AddWarning("Attempt to allocate already-allocated address {0:x16}", addr);
+                MetaData.AddWarning("Attempt to allocate already-allocated address {0:x16} size {0:x16}", addr, size);
               }
             }
             break;
@@ -529,8 +530,11 @@ namespace MemTrace
           return false;
         }
 
-        if (!MetaData.Symbols.Contains(frames[i]))
+        if (!SymbolsHashSet.Contains(frames[i]))
+        {
           MetaData.Symbols.Add(frames[i]);
+          SymbolsHashSet.Add(frames[i]);
+        }
       }
 
       ++m_SeenStackRollback;
