2be92b1d0c23bdd9552815ac8d8681c27cfe2799
gjaegy <g.jaegy@imagine3d.fr>
2020-02-06 17:13:42 +0100

    increased socket send buffer size to avoid exception

--- runtime/MemTrace.cpp
+++ runtime/MemTrace.cpp
@@ -92,10 +92,11 @@ namespace MemTrace
   // Various limits
   enum
   {
-    kBufferSize     = 32768,
-    kMaxStrings     = 1024,   // Max string hashes to keep around
-    kMaxStacks      = 1024,   // Max call stack hashes to keep around
-    kMaxFrames      = 256,    // Max frames in single callstack - needs to be large to capture all of it
+    kBufferSize            = 32768,
+    kMaxStrings            = 1024,   // Max string hashes to keep around
+    kMaxStacks             = 1024,   // Max call stack hashes to keep around
+    kMaxFrames             = 256,    // Max frames in single callstack - needs to be large to capture all of it
+    kSocketSendBufferSize  = 4194304 // Socket send buffer size
   };
 
   // Start of stream protocol value - to handle version changes without crashing decoder.
